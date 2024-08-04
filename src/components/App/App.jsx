1a5e50dcb5ca0f8764939453974cb80e949fa038
Anastasiia Semernya <semernya38@gmail.com>
2024-08-04 18:08:31 +0300

    add lazy, suspence

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,11 +9,13 @@ import css from "./App.module.css";
 // import { useEffect } from 'react';
 // import { fetchContacts } from '../../redux/contactsOps';
 import { NavLink, Route, Routes } from "react-router-dom";
-import HomePage from '../../pages/HomePage/HomePage'
-import RegistrationPage from '../../pages/RegistrationPage/RegistrationPage'
-import LoginPage from "../../pages/LoginPage/LoginPage";
-import ContactsPage from "../../pages/ContactsPage/ContactsPage";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+import { lazy, Suspense } from "react";
+import Layout from "../Layout/Layout";
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'))
+const RegistrationPage = lazy(() => import('../../pages/RegistrationPage/RegistrationPage'))
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage'))
+const ContactsPage = lazy(() => import('../../pages/ContactsPage/ContactsPage'))
+const NotFoundPage = lazy(() => import('../../pages/NotFoundPage/NotFoundPage'))
 
 export default function App() {
 
@@ -35,22 +37,23 @@ export default function App() {
     //   {error && <Error />}
     //   <ContactList />
     // </div>
+    <Layout>
+      <Suspense fallback={null}>
+        <nav className={css.nav}>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/register'>SignIn</NavLink>
+          <NavLink to='/login'>LogIn</NavLink>
+          <NavLink to='/contacts'>Contacts</NavLink>
+        </nav>
 
-    <div>
-      <nav className={css.nav}>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/register'>SignIn</NavLink>
-        <NavLink to='/login'>LogIn</NavLink>
-        <NavLink to='/contacts'>Contacts</NavLink>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </Layout>
   )
 }
